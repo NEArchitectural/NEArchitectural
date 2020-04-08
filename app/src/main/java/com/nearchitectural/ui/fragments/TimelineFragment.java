615960d90a460fd2dbb22fa5ad1ceea292f39289
JamesAllwoodPanter <43777338+JamesAllwoodPanter@users.noreply.github.com>
2020-04-08 22:11:27 +0100

    Ui design for about page

--- app/src/main/java/com/nearchitectural/ui/fragments/TimelineFragment.java
+++ app/src/main/java/com/nearchitectural/ui/fragments/TimelineFragment.java
@@ -9,6 +9,11 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+<<<<<<< Updated upstream
+=======
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
+>>>>>>> Stashed changes
 import com.nearchitectural.R;
 import com.nearchitectural.ui.activities.MapsActivity;
 
@@ -22,11 +27,47 @@ public class TimelineFragment extends Fragment {
 
     public static final String TAG = "TimelineFragment";
 
+<<<<<<< Updated upstream
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_timeline,container,false);
     }
+=======
+    private ImageView thumbnail;
+    private TextView title;
+    private FirebaseFirestore db; // Represents the database storing location information
+    private Query dbq;
+    private Bundle arguments;
+    private TextView summaryTimeLine;
+    private TimeLine timeLine;
+    private TextView yearOpened;
+    private FragmentTimelineBinding timeLineBinding;
+
+
+    public TimeLine getTimeLine() {
+        return timeLine;
+    }
+
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        arguments = getArguments();
+
+        timeLineBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_timeline, container, false);
+        timeLineBinding.setModel(timeLine); // Set selected location as data binding model
+
+        title = timeLineBinding.timelineTitle;
+        summaryTimeLine = timeLineBinding.timelineSummary;
+        thumbnail = timeLineBinding.thumbnail;
+        yearOpened = timeLineBinding.yearOpened;
+
+        return inflater.inflate(R.layout.fragment_timeline,container,false);
+}
+
+>>>>>>> Stashed changes
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
@@ -34,5 +75,24 @@ public class TimelineFragment extends Fragment {
         MapsActivity parentActivity = (MapsActivity) this.getActivity();
         parentActivity.getNavigationView().getMenu().findItem(R.id.nav_timeline).setChecked(true);
         parentActivity.setActionBarTitle("Timeline");
+<<<<<<< Updated upstream
+=======
+
+    }
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+
+
+>>>>>>> Stashed changes
     }
 }
+<<<<<<< Updated upstream
+=======
+
+
+
+
+>>>>>>> Stashed changes
